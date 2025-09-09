e6ad22c9dbc4c07317b7d09293115caca6b013f4
Moruan06 <moruan.mattar@gmail.com>
2025-09-09 19:59:34 -0300

    feat: implement navigation functionality with event listeners for home, menu, and contact buttons

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "./style.css"
+import {Home} from "./home.js";
+
+const content = document.querySelector("#content");
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const contactButton = document.querySelector("#contato");
+
+// Home.homePage(content);
+
+const removeClass = () => {
+    homeButton.classList.remove("active")
+    menuButton.classList.remove("active")
+    contactButton.classList.remove("active")
+}
+
+homeButton.addEventListener("click", () => {
+    removeClass();
+    homeButton.classList.add("active");
+    Home.homePage(content);
+})
+
+menuButton.addEventListener("click", () => {
+    removeClass();
+    menuButton.classList.add("active");
+    Menu.menuPage(content);
+})
+
+contactButton.addEventListener("click", () => {
+    removeClass();
+    contactButton.classList.add("active");
+    Contact.contactPage(content);
+})
